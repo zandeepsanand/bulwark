af4f61c239e06d4681aab5e6e3c3ea4ccdc90c10
zandeepsanand <sandeepsssanand@gmail.com>
2022-04-04 19:04:40 +0530

    img

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const main = document.getElementById('main');
 let parts = [];
 
 let images = [
-  "/src/img/1.png",
+  "src/img/1.png",
   "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
   "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
 ];
